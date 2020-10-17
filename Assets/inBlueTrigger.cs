c3f7142b2991ae951e082f797b985265640f6e8b
Cassie Hui <cch542@nyu.edu>
2020-10-17 18:30:01 -0400

    start of dream mechanic
    pig + blue trigger with dream score

---
+++ Assets/inBlueTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inBlueTrigger : MonoBehaviour
+{
+
+    public int dreampoints = 0;
+
+    public Text pointsDreamText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D pig)
+    {
+        if(pig.tag == "dreampig")
+        {
+
+            dreampoints++;
+            pointsDreamText.text = ("dream stuffies: " + dreampoints.ToString());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D pig)
+    {
+        if (pig.tag == "dreampig")
+        {
+
+            dreampoints--;
+            pointsDreamText.text = ("dream stuffies: " + dreampoints.ToString());
+        }
+    }
+}
